5691b39b2e685105ccb87c67181cfe21c679b375
Milan Burmaja <burmajam@gmail.com>
2016-03-31 15:27:05 +0200

    Removed struct from common functions

--- lib/aggregate.ex
+++ lib/aggregate.ex
@@ -22,27 +22,27 @@ defmodule Aggregate do
   def init(:ok), do: {:ok, %State{buffer: [], msg: "", ttl: 1_000}}
   ## end custom
 
-  def handle_call({:cmd, cmd}, from, %State{buffer: [], transaction: nil}=state) do
+  def handle_call({:cmd, cmd}, from, %{buffer: [], transaction: nil}=state) do
     Logger.debug "Executing: #{inspect cmd}"
     lock = make_ref
     GenServer.cast self, {:execute, {cmd, from}}
-    {:noreply, %State{state | transaction: lock}}
+    {:noreply, %{state | transaction: lock}}
   end
-  def handle_call({:cmd, cmd}, from, %State{}=state) do
+  def handle_call({:cmd, cmd}, from, %{}=state) do
     Logger.debug "Buffering: #{inspect cmd}"
     buffer = [{cmd, from} | state.buffer]
-    {:noreply, %State{state | buffer: buffer}}
+    {:noreply, %{state | buffer: buffer}}
   end
   def handle_call({:commit, nil, _events}, _from, state) do 
     {:reply, {:error, :nil_transaction}, state}
   end
-  def handle_call({:commit, transaction, events}, _from, %State{transaction: transaction}=state) do
+  def handle_call({:commit, transaction, events}, _from, %{transaction: transaction}=state) do
     Logger.debug "Commiting: #{inspect transaction}"
     state = apply_events events, state
     GenServer.cast self, :process_buffer
-    {:reply, :ok, %State{state | transaction: nil, events: []}}
+    {:reply, :ok, %{state | transaction: nil, events: []}}
   end
-  def handle_call({:commit, t1, _events}, _from, %State{transaction: transaction}=state) do 
+  def handle_call({:commit, t1, _events}, _from, %{transaction: transaction}=state) do 
     {:reply, {:error, :wrong_transaction}, state}
   end
 
@@ -65,21 +65,21 @@ defmodule Aggregate do
   end
   ## end custom
 
-  def handle_cast(:process_buffer, %State{buffer: []}=state), do: {:noreply, state}
-  def handle_cast(:process_buffer, %State{buffer: buffer, transaction: nil}=state) do
+  def handle_cast(:process_buffer, %{buffer: []}=state), do: {:noreply, state}
+  def handle_cast(:process_buffer, %{buffer: buffer, transaction: nil}=state) do
     lock = make_ref
     {cmd, from} = List.last buffer
     Logger.debug "Processing buffered cmd: #{inspect cmd}"
     buffer = List.delete_at buffer, -1
     GenServer.cast self, {:execute, {cmd, from}}
-    {:noreply, %State{state | buffer: buffer, transaction: lock}}
+    {:noreply, %{state | buffer: buffer, transaction: lock}}
   end
   def handle_cast(:process_buffer, state), do: {:noreply, state}
 
-  def handle_info({:rollback, transaction}, %State{transaction: transaction}=state) do 
+  def handle_info({:rollback, transaction}, %{transaction: transaction}=state) do 
     Logger.warn "Rolling back transaction #{inspect transaction}"
     GenServer.cast self, :process_buffer
-    {:noreply, %State{state | transaction: nil}}
+    {:noreply, %{state | transaction: nil}}
   end
   def handle_info(_, state), do: {:noreply, state}
 
