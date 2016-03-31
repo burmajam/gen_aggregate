0b41dfc5c09c179eaa66483a2fa7446dac1fbaea
Milan Burmaja <burmajam@gmail.com>
2016-03-31 16:39:08 +0200

    moved Aggregate module in test folder

--- lib/gen_aggregate.ex
+++ lib/gen_aggregate.ex
@@ -7,7 +7,7 @@ defmodule GenAggregate do
   defmacro __using__(_) do
     quote do
       use GenServer
-      require Logger
+      #require Logger
 
       def commit(pid, transaction), do: GenServer.call(pid, {:commit, transaction})
 
@@ -16,13 +16,13 @@ defmodule GenAggregate do
       def reply(to, payload), do: GenServer.reply to, payload
 
       def handle_call({:cmd, cmd}, from, %{buffer: [], transaction: nil}=state) do
-        Logger.debug "Executing: #{inspect cmd}"
+        #Logger.debug "Executing: #{inspect cmd}"
         lock = make_ref
         GenServer.cast self, {:execute, {cmd, from}}
         {:noreply, %{state | transaction: lock}}
       end
       def handle_call({:cmd, cmd}, from, %{}=state) do
-        Logger.debug "Buffering: #{inspect cmd}"
+        #Logger.debug "Buffering: #{inspect cmd}"
         buffer = [{cmd, from} | state.buffer]
         {:noreply, %{state | buffer: buffer}}
       end
@@ -30,7 +30,7 @@ defmodule GenAggregate do
       {:reply, {:error, :nil_transaction}, state}
       end
       def handle_call({:commit, transaction}, _from, %{transaction: transaction}=state) do
-        Logger.debug "Commiting: #{inspect transaction}"
+        #Logger.debug "Commiting: #{inspect transaction}"
         state = apply_events state.events, state
         GenServer.cast self, :process_buffer
         {:reply, :ok, %{state | transaction: nil, events: []}}
@@ -43,7 +43,7 @@ defmodule GenAggregate do
       def handle_cast(:process_buffer, %{buffer: buffer, transaction: nil}=state) do
         lock = make_ref
         {cmd, from} = List.last buffer
-        Logger.debug "Processing buffered cmd: #{inspect cmd}"
+        #Logger.debug "Processing buffered cmd: #{inspect cmd}"
         buffer = List.delete_at buffer, -1
         GenServer.cast self, {:execute, {cmd, from}}
         {:noreply, %{state | buffer: buffer, transaction: lock}}
@@ -54,7 +54,7 @@ defmodule GenAggregate do
       end
 
       def handle_info({:rollback, transaction}, %{transaction: transaction}=state) do 
-      Logger.warn "Rolling back transaction #{inspect transaction}"
+      #Logger.warn "Rolling back transaction #{inspect transaction}"
       GenServer.cast self, :process_buffer
       {:noreply, %{state | transaction: nil}}
       end
--- test/gen_aggregate_test.exs
+++ test/gen_aggregate_test.exs
@@ -1,6 +1,49 @@
-defmodule AggregateTest do
+defmodule Aggregate do
+  use GenAggregate
+  require Logger
+
+  defmodule State, do: defstruct [:transaction, :ttl, :events, :buffer, :msg]
+
+  ## Client API
+
+  def start_link(ttl \\ 2_000), do: GenAggregate.start_link __MODULE__, ttl
+
+  def do_something(pid, val) do 
+    Logger.debug "Try #{val}"
+    exec pid, {:do_something, val}
+  end
+
+  def message(pid), do: exec pid, :get_message
+
+
+  ## Server Callbacks
+
+  def init(ttl), do: {:ok, %State{buffer: [], msg: "", ttl: ttl}}
+
+  def handle_exec({:do_something, val}, from, state) do
+    Logger.debug "Doing #{val}"
+    events = [%{val: val}]
+    result = {:ok, state.transaction, events}
+    Logger.debug "Result sent: #{inspect result}"
+
+    schedule_rollback state.transaction, state.ttl
+    reply from, result
+    {:noreply, %{state | events: events}}
+  end
+  def handle_exec(:get_message, from, state) do
+    reply from, state.msg
+    {:noreply, %{state | transaction: nil}}
+  end
+
+  defp apply_events([%{val: val} | tail], state) do
+    state = %{ state | msg: state.msg <> val }
+    apply_events tail, state
+  end
+end
+
+defmodule GenAggregateTest do
   use ExUnit.Case, async: true
-  doctest Aggregate
+  doctest GenAggregate
 
   setup do
     {:ok, a} = Aggregate.start_link
