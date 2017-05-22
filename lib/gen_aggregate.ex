2dd629e9092acc678a60961b3c67ea936aca0b6c
Milan Burmaja <burmajam@gmail.com>
2017-05-22 16:48:14 +0200

    Shutdown aggregate if transaction timeouts

--- lib/gen_aggregate.ex
+++ lib/gen_aggregate.ex
@@ -64,8 +64,8 @@ defmodule GenAggregate do
 
       def handle_call({:cmd, cmd}, from, %{buffer: [], transaction: nil}=state) do
         #Logger.debug "Executing: #{inspect cmd}"
-        lock = make_ref
-        GenServer.cast self, {:execute, {cmd, from}}
+        lock = make_ref()
+        GenServer.cast self(), {:execute, {cmd, from}}
         {:noreply, %{state | transaction: lock}}
       end
       def handle_call({:cmd, cmd}, from, %{}=state) do
@@ -79,7 +79,7 @@ defmodule GenAggregate do
       def handle_call({:commit, transaction}, _from, %{transaction: transaction}=state) do
         #Logger.debug "Commiting: #{inspect transaction}"
         state = apply_events state.events, state
-        GenServer.cast self, :process_buffer
+        GenServer.cast self(), :process_buffer
         {:reply, :ok, %{state | transaction: nil, events: []}}
       end
       def handle_call({:commit, t1}, _from, %{transaction: transaction}=state) do 
@@ -93,11 +93,11 @@ defmodule GenAggregate do
 
       def handle_cast(:process_buffer, %{buffer: []}=state), do: {:noreply, state}
       def handle_cast(:process_buffer, %{buffer: buffer, transaction: nil}=state) do
-        lock = make_ref
+        lock = make_ref()
         {cmd, from} = List.last buffer
         #Logger.debug "Processing buffered cmd: #{inspect cmd}"
         buffer = List.delete_at buffer, -1
-        GenServer.cast self, {:execute, {cmd, from}}
+        GenServer.cast self(), {:execute, {cmd, from}}
         {:noreply, %{state | buffer: buffer, transaction: lock}}
       end
       def handle_cast(:process_buffer, state), do: {:noreply, state}
@@ -118,14 +118,14 @@ defmodule GenAggregate do
       end
 
       def handle_info({:rollback, transaction}, %{transaction: transaction}=state) do 
-      #Logger.warn "Rolling back transaction #{inspect transaction}"
-      GenServer.cast self, :process_buffer
-      {:noreply, %{state | transaction: nil}}
+        #Logger.warn "Rolling back transaction #{inspect transaction}"
+        #GenServer.cast self(), :process_buffer
+        {:stop, {:commit_timeout, state}, state}
       end
       def handle_info(_, state), do: {:noreply, state}
 
       defp schedule_rollback(transaction, ttl) do 
-        {:ok, _ref} = :timer.send_after ttl, self, {:rollback, transaction}
+        {:ok, _ref} = :timer.send_after ttl, self(), {:rollback, transaction}
       end
 
       defp apply_events([], state), do: state
--- mix.exs
+++ mix.exs
@@ -3,14 +3,34 @@ defmodule Aggregate.Mixfile do
 
   def project do
     [app: :aggregate,
-     version: "0.0.4",
+     version: "0.0.5",
      elixir: "~> 1.2",
+     package: package(),
+     description: description(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps()]
   end
 
   def application, do: [applications: [:logger]]
 
-  defp deps, do: []
+  defp deps do 
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp package do
+    %{
+       maintainers: ["Milan Burmaja"],
+       links: %{ "GitHub" => "https://github.com/burmajam/gen_aggregate"},
+       licenses: ["MIT"],
+       files: ~w(lib mix.exs README*) }
+  end
+
+  defp description do
+    """
+    Aggregate from DDD. Perfect fit with Extreme project
+    """
+  end
 end
